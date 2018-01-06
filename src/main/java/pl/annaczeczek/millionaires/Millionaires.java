c35229714b81b894d5c4af25744a0aebb6cfb0d3
aczeczek <ania.czeczek@gmail.com>
2018-01-06 12:29:26 +0100

    Set port

--- src/main/java/pl/annaczeczek/millionaires/Millionaires.java
+++ src/main/java/pl/annaczeczek/millionaires/Millionaires.java
@@ -3,12 +3,20 @@ package pl.annaczeczek.millionaires;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableScheduling
 @SpringBootApplication
 public class Millionaires {
 
     public static void main(String[] args) {
         SpringApplication.run(Millionaires.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
     }
 }
